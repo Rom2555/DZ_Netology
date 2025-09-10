868ef7ba1cb0de2437f1a1fa7a3616a8852a8bc8
Roman Egorov <rk2555@yandex.ru>
2025-09-10 14:12:14 +0300

    В файле oop_1.py созданы классы Lecturer и Reviewer, наследованные от класса Mentor. Метод rate_hw перенесен из класса Mentor в класс Reviewer. Обновлены проверочные тесты.

--- OOP/oop_1.py
+++ OOP/oop_1.py
@@ -17,10 +17,20 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
-                student.grades[course] += [grade]
+                student.grades[course].append(grade)
                 return None
             else:
                 student.grades[course] = [grade]
@@ -33,9 +43,18 @@ best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 cool_mentor = Mentor('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
+reviewer = Reviewer('S', 'B')
+reviewer.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+reviewer.rate_hw(best_student, 'Python', 10)
+reviewer.rate_hw(best_student, 'Python', 10)
+reviewer.rate_hw(best_student, 'Python', 10)
 
 print(best_student.grades)
+
+lecturer = Lecturer('Иван', 'Иванов')
+reviewer = Reviewer('Пётр', 'Петров')
+print(isinstance(lecturer, Mentor))  # True
+print(isinstance(reviewer, Mentor))  # True
+print(lecturer.courses_attached)  # []
+print(reviewer.courses_attached)  # []
